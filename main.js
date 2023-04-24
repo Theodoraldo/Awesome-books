d4fce98fb7eacb37cd61fa57f9b60fb2bf502913
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-04-24 10:03:15 +0000

    Update main.js file

---
+++ main.js
@@ -0,0 +1,75 @@
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const form = document.querySelector('form');
+const displayBooks = document.querySelector('.container');
+
+// Is localStorage Empty
+if (localStorage.getItem('Book List') === null) {
+  localStorage.setItem('Book List', JSON.stringify([]));
+}
+
+// Data Storage into localStorage
+const dataStore = JSON.parse(localStorage.getItem('Book List'));
+
+// localStorage data update
+function updateData() {
+  localStorage.setItem('Book List', JSON.stringify(dataStore));
+}
+
+//Fetching data from localStorage
+function fetchData() {
+  displayBooks.innerHTML = `
+    <ul>
+      ${createBooks(dataStore)}
+    </ul>
+  `;
+}
+
+// Create data row
+function createBooks(items) {
+  let books = '';
+  for (let i = 0; i < items.length; i += 1) {
+    books += `
+      <p>${items[i].title}</p>
+      <p>${items[i].author}</p>
+      <button onclick='removeItem(${i})'>Remove</button>
+      <hr />
+    `;
+  }
+  return books;
+}
+
+// Adding new data to localStorage
+function newData(bookTitle, bookAuthor) {
+  const Books = {
+    title: bookTitle,
+    author: bookAuthor,
+  }
+  dataStore.push(Books);
+  updateData();
+  fetchData();
+}
+
+// Removing  data from localStorage
+function removeItem(index) {
+  dataStore.splice(index, 1);
+  updateData();
+  fetchData();
+}
+
+// Getting stored data from localStorage
+form.onsubmit = (e) => {
+  if (title.value === '') {
+    alert('Please enter title of book');
+    e.preventDefault()
+  }
+  else if (author.value === '') {
+    alert('Please enter author of book');
+    e.preventDefault()
+  }
+  else {
+    newData(title.value, author.value);
+  }
+};
+
+window.onload = fetchData();
